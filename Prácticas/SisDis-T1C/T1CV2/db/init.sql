05f8ab70ab29dd0483648ab5f0570b74947d1f53
RSS1013 <rss1013@alu.ubu.es>
2025-05-12 23:50:21 +0200

    subo de nuevo la v2
    subo de nuevo v2

---
+++ Prácticas/SisDis-T1C/T1CV2/db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS sistema_distribuido;
+USE sistema_distribuido;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+INSERT INTO usuarios (username, password) VALUES ('RSadmin', 'RSadmin123');
---
+++ Prácticas/SisDis-T1C/T1CV2/frontend-springboot/src/main/java/Ricardo/SisDisP2/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package Ricardo.SisDisP2.repository;
+
+import Ricardo.SisDisP2.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
