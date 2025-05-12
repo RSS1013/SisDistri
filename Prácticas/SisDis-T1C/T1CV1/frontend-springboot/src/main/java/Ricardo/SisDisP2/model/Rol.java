c0d5986c2b2c06965ab8715754ba1d28e69ded98
RSS1013 <rss1013@alu.ubu.es>
2025-05-12 02:10:35 +0200

    cambio en el pom a la 3.0.0 y Todo el tema de DTO y repository
    Cambio a la versión 3.0.0 de srpingboot teníamos la 3.1.x y pasamos a 3.0.0, adicionalmente creamos la estructura de DTO y otros añadidos para implementar la seguridad de srpingsecurity

---
+++ Prácticas/SisDis-T1C/T1CV1/frontend-springboot/src/main/java/Ricardo/SisDisP2/model/Rol.java
@@ -0,0 +1,24 @@
+package Ricardo.SisDisP2.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    public Rol() {}
+
+    public Rol(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+}
